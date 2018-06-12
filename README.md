d4b676c33b7bfe5c1f165e0fc38ca501bfd2bd34
wanghaoming <whimmy@126.com>
2018-06-12 11:50:32 +0800

    add java8-checker

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 Module | Article
 --|--
 java8-locale | [Internationalization and Localization in Java 8](http://www.baeldung.com/java-8-localization)
+java8-checker | [The Checker Framework – Pluggable Type Systems for Java](http://www.baeldung.com/checker-framework)
---
+++ java8-checker/src/main/java/com/ymmihw/java8/checker/RegexExample.java
@@ -0,0 +1,20 @@
+package com.ymmihw.java8.checker;
+
+import org.checkerframework.checker.regex.qual.Regex;
+
+public class RegexExample {
+
+  // For some reason we want to be sure that this regex
+  // contains at least one capturing group.
+  // However, we do an error and we forgot to define such
+  // capturing group in it.
+  @Regex(1)
+  private static String findNumbers = "\\d*";
+
+  public static void main(String[] args) {
+    String message = "My phone number is +3911223344.";
+    message.matches(findNumbers);
+  }
+
+}
+  
